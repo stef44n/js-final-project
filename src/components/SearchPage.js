89033ed1dd27d775e04230380affd4a3b56e39a3
Stefan <stefanseifeddine@gmail.com>
2023-07-16 00:05:02 +0100

    Display correct data matching each video

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,12 +1,52 @@
 import React, { useEffect, useState } from "react";
 import "../styles/SearchPage.css";
 import mockData from "../testData/mockData";
-import API_KEY from "../testData/apikey";
+import mockDataSearchChannels from "../testData/mockDataSearchChannels";
+import mockDataSearchVideosByVidId from "../testData/mockDataSearchVideosByVidId";
+// import API_KEY from "../testData/apikey";
+import { formatNumber, calculateTimeDifference } from "./HelperFunctions";
 
 // SEARCH PAGE
 export default function Homepage() {
     // const [apiData, setApiData] = useState([]);
     const [apiData, setApiData] = useState(mockData.items);
+
+    // Requires 3 different API requests (100 + 1 + 1 tokens)
+    const allChannels = mockDataSearchChannels;
+    const allVideos = mockDataSearchVideosByVidId;
+
+    const channelIDs = apiData.map((video) => video.snippet.channelId);
+    const videoIDs = apiData.map((video) => video.id.videoId);
+    console.log(channelIDs);
+    console.log(videoIDs);
+
+    const commaSeparatedString = channelIDs.join(",");
+    const commaSeparatedVideos = videoIDs.join(",");
+    console.log(commaSeparatedString);
+    console.log(commaSeparatedVideos);
+
+    const allChannelsIDs = allChannels.items.map((video) => video.id);
+    const allVideoIDs = allVideos.items.map((video) => video.id);
+    console.log(allChannelsIDs);
+    console.log(allVideoIDs);
+
+    const getAvatarForChannelId = (channelId) => {
+        console.log(channelId);
+        const matchedAvatarIndex = allChannelsIDs.findIndex(
+            (avatar) => avatar === channelId
+        );
+        console.log(matchedAvatarIndex);
+        return matchedAvatarIndex;
+    };
+
+    const getInfoForVideoId = (videoId) => {
+        console.log(videoId);
+        const matchedVideoIndex = allVideoIDs.findIndex(
+            (info) => info === videoId
+        );
+        console.log(matchedVideoIndex);
+        return matchedVideoIndex;
+    };
 
     const getData = async () => {
         try {
@@ -71,9 +111,29 @@ export default function Homepage() {
                     <div className="video-info">
                         <div className="video-info">
                             <h2>{video.snippet.title}</h2>
-                            <p>View count / Release date</p>
+                            <p>
+                                {formatNumber(
+                                    allVideos.items[
+                                        getInfoForVideoId(videoIDs[index])
+                                    ]?.statistics.viewCount
+                                )}{" "}
+                                views •{" "}
+                                {calculateTimeDifference(
+                                    video.snippet.publishedAt
+                                )}{" "}
+                            </p>
                             <div>
-                                <div className="channel-avatar">ava</div>
+                                <img
+                                    className="channel-avatar"
+                                    alt="avatar"
+                                    src={
+                                        allChannels.items[
+                                            getAvatarForChannelId(
+                                                channelIDs[index]
+                                            )
+                                        ]?.snippet.thumbnails.default.url
+                                    }
+                                ></img>
                                 <p>{video.snippet.channelTitle}</p>
                             </div>
                             <p className="video-desc">
@@ -87,28 +147,3 @@ export default function Homepage() {
         </div>
     );
 }
-
-// {
-/* <div className="yt-grid">
-    {apiData.map((video, index) => (
-        <article key={index}>
-            <img
-                className="thumbnail"
-                // src={`${getData()}`}
-                src={video.snippet.thumbnails.high.url}
-                alt="thumbnail"
-            />
-            <div className="below-tn">
-                <div className="channel-avatar">ava</div>
-                <div className="video-info">
-                    <h2>{video.snippet.title}</h2>
-                    <p>{video.snippet.channelTitle}</p>
-                    <p>View count / Release date</p>
-                    <p>{video.snippet.description}</p>
-                </div>
-            </div>
-        </article>
-    ))}
-    {getData}
-</div>; */
-// }
